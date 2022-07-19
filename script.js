c35e09bf871cf241c29b8e38e4ae6eae601fbb23
napalm-git <frozenrox22@gmail.com>
2022-07-19 17:06:22 +0300

    Removed unused code

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
 function getComputerChoice(rock, paper, scissor) {
-  // let x = rock;
-  // let y = paper;
-  // let z = scissor;
-
   let arrayOfChoices = [rock, paper, scissor];
 
   let randomChoice = Math.floor(Math.random() * arrayOfChoices.length);
 
   let computerChoice = arrayOfChoices[randomChoice];
-
-  // if (choice[0]) {
-  //   console.log("Rock");
-  // } else if (choice[1]) {
-  //   console.log("Paper");
-  // } else if (choice[2]) {
-  //   console.log("Scissor");
-  // }
 
   return computerChoice;
 }
