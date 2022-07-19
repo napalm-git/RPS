c47cd2de50e7c98b82a96ef7e77a7e4c5e8e269d
napalm-git <frozenrox22@gmail.com>
2022-07-19 17:47:46 +0300

    Add playRound and the game() functions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Gets computers choice
 function getComputerChoice(rock, paper, scissor) {
   let arrayOfChoices = [rock, paper, scissor];
 
@@ -8,4 +9,32 @@ function getComputerChoice(rock, paper, scissor) {
   return computerChoice;
 }
 
-console.log(getComputerChoice("rock", "paper", "scissor"));
+// Plays a single round
+function playRound(playerSelection, computerSelection) {
+  switch (true) {
+    case playerSelection == "Rock" && computerSelection == "Rock":
+      console.log("its a draw");
+      break;
+
+    case playerSelection == "Rock" && computerSelection == "Paper":
+      console.log("Paper wins");
+      break;
+
+    case playerSelection == "Rock" && computerSelection == "Scissor":
+      console.log("Rock wins");
+      break;
+
+    default:
+      console.log("No one wins");
+      break;
+  }
+}
+
+// Plays the game
+function game() {
+  for (let i = 0; i < 5; i++) {
+    playRound("Rock", getComputerChoice("Rock", "Paper", "Scissor"));
+  }
+}
+
+game();
