449c883cf4df6d9941bcb2c0b80b84624f8aec88
napalm-git <frozenrox22@gmail.com>
2022-07-24 18:14:55 +0300

    Some more changes

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const container = document.querySelector(".container");
 const btnRock = document.querySelector(".btnRock");
 const btnPaper = document.querySelector(".btnPaper");
 const btnScissor = document.querySelector(".btnScissor");
+const btnRestart = document.querySelector(".btnRestart");
 const player = document.querySelector(".player-score");
 const cpu = document.querySelector(".cpu-score");
 const playerChoice = document.querySelector(".player-choice");
 const cpuChoice = document.querySelector(".cpu-choice");
 const result = document.querySelector(".result");
 const endGame = document.querySelector(".endGame");
+const start = document.querySelector(".start");
 
 container.appendChild(playerChoice);
 container.appendChild(cpuChoice);
@@ -15,6 +17,14 @@ container.appendChild(result);
 container.appendChild(player);
 container.appendChild(cpu);
 container.appendChild(endGame);
+container.appendChild(start);
+
+start.innerText = "Click one of the buttons to start playing!";
+playerChoice.style.cssText = "none";
+cpuChoice.style.cssText = "none";
+result.style.cssText = "none";
+player.style.cssText = "none";
+cpu.style.cssText = "none";
 
 let playerScore = 0;
 let cpuScore = 0;
@@ -33,93 +43,149 @@ function getComputerChoice(rock, paper, scissor) {
 function playRound(playerSelection, computerSelection) {
   switch (true) {
     // Draw Conditions
-    case playerSelection.toLowerCase() == "rock" && computerSelection == "rock":
+    case playerSelection == "rock" && computerSelection == "rock":
+      start.innerText = "";
+      start.style.cssText = "background: none";
+      playerChoice.style.cssText = "background-color: aqua";
       playerChoice.innerText = `PLAYER CHOICE: 💎`;
+      cpuChoice.style.cssText = "background-color: aquamarine";
       cpuChoice.innerText = `CPU CHOICE: 💎`;
+      result.style.cssText = "background-color: chartreuse";
       result.innerText = `ITS A DRAW`;
       player.innerText = `PLAYER: ${playerScore}`;
+      player.style.cssText = "background-color: cyan";
       cpu.innerText = `CPU: ${cpuScore}`;
+      cpu.style.cssText = "background-color: violet";
       break;
 
-    case playerSelection.toLowerCase() == "paper" &&
-      computerSelection == "paper":
+    case playerSelection == "paper" && computerSelection == "paper":
+      start.innerText = "";
+      start.style.cssText = "background: none";
+      playerChoice.style.cssText = "background-color: aqua";
       playerChoice.innerText = `PLAYER CHOICE: 🧻`;
       cpuChoice.innerText = `CPU CHOICE: 🧻`;
+      cpuChoice.style.cssText = "background-color: aquamarine";
+      result.style.cssText = "background-color: chartreuse";
       result.innerText = `ITS A DRAW`;
       player.innerText = `PLAYER: ${playerScore}`;
+      player.style.cssText = "background-color: cyan";
       cpu.innerText = `CPU: ${cpuScore}`;
+      cpu.style.cssText = "background-color: violet";
       break;
 
-    case playerSelection.toLowerCase() == "scissor" &&
-      computerSelection == "scissor":
+    case playerSelection == "scissor" && computerSelection == "scissor":
+      start.innerText = "";
+      start.style.cssText = "background: none";
+      playerChoice.style.cssText = "background-color: aqua";
       playerChoice.innerText = `PLAYER CHOICE: ✂`;
       cpuChoice.innerText = `CPU CHOICE: ✂`;
+      cpuChoice.style.cssText = "background-color: aquamarine";
       result.innerText = `ITS A DRAW`;
+      result.style.cssText = "background-color: chartreuse";
       player.innerText = `PLAYER: ${playerScore}`;
+      player.style.cssText = "background-color: cyan";
       cpu.innerText = `CPU: ${cpuScore}`;
+      cpu.style.cssText = "background-color: violet";
       break;
 
     // Rock win conditions
-    case playerSelection.toLowerCase() == "rock" &&
-      computerSelection == "scissor":
+    case playerSelection == "rock" && computerSelection == "scissor":
+      start.innerText = "";
+      start.style.cssText = "background: none";
+      playerChoice.style.cssText = "background-color: aqua";
       playerChoice.innerText = `PLAYER CHOICE: 💎`;
       cpuChoice.innerText = `CPU CHOICE: ✂`;
+      cpuChoice.style.cssText = "background-color: aquamarine";
       result.innerText = `💎 WINS`;
+      result.style.cssText = "background-color: chartreuse";
       playerScore++;
       player.innerText = `PLAYER: ${playerScore}`;
+      player.style.cssText = "background-color: cyan";
       cpu.innerText = `CPU: ${cpuScore}`;
+      cpu.style.cssText = "background-color: violet";
       break;
 
-    case playerSelection.toLowerCase() == "scissor" &&
-      computerSelection == "rock":
+    case playerSelection == "scissor" && computerSelection == "rock":
+      start.innerText = "";
+      start.style.cssText = "background: none";
+      playerChoice.style.cssText = "background-color: aqua";
       playerChoice.innerText = `PLAYER CHOICE: ✂`;
       cpuChoice.innerText = `CPU CHOICE: 💎`;
+      cpuChoice.style.cssText = "background-color: aquamarine";
       result.innerText = `ROCK WINS`;
+      result.style.cssText = "background-color: chartreuse";
       cpuScore++;
       player.innerText = `PLAYER: ${playerScore}`;
+      player.style.cssText = "background-color: cyan";
       cpu.innerText = `CPU: ${cpuScore}`;
+      cpu.style.cssText = "background-color: violet";
       break;
 
     // Paper win conditions
-    case playerSelection.toLowerCase() == "paper" &&
-      computerSelection == "rock":
+    case playerSelection == "paper" && computerSelection == "rock":
+      start.innerText = "";
+      start.style.cssText = "background: none";
+      playerChoice.style.cssText = "background-color: aqua";
       playerChoice.innerText = `PLAYER CHOICE: 🧻`;
       cpuChoice.innerText = `CPU CHOICE: 💎`;
+      cpuChoice.style.cssText = "background-color: aquamarine";
       result.innerText = `🧻 WINS`;
+      result.style.cssText = "background-color: chartreuse";
       playerScore++;
       player.innerText = `PLAYER: ${playerScore}`;
+      player.style.cssText = "background-color: cyan";
       cpu.innerText = `CPU: ${cpuScore}`;
+      cpu.style.cssText = "background-color: violet";
       break;
 
-    case playerSelection.toLowerCase() == "rock" &&
-      computerSelection == "paper":
+    case playerSelection == "rock" && computerSelection == "paper":
+      start.innerText = "";
+      start.style.cssText = "background: none";
+      playerChoice.style.cssText = "background-color: aqua";
       playerChoice.innerText = `PLAYER CHOICE: 💎`;
       cpuChoice.innerText = `CPU CHOICE: 🧻`;
+      cpuChoice.style.cssText = "background-color: aquamarine";
       result.innerText = `🧻 WINS`;
+      result.style.cssText = "background-color: chartreuse";
       cpuScore++;
       player.innerText = `PLAYER: ${playerScore}`;
+      player.style.cssText = "background-color: cyan";
       cpu.innerText = `CPU: ${cpuScore}`;
+      cpu.style.cssText = "background-color: violet";
       break;
 
     // Scissor win conditions
-    case playerSelection.toLowerCase() == "scissor" &&
-      computerSelection == "paper":
+    case playerSelection == "scissor" && computerSelection == "paper":
+      start.innerText = "";
+      start.style.cssText = "background: none";
+      playerChoice.style.cssText = "background-color: aqua";
       playerChoice.innerText = `PLAYER CHOICE: ✂`;
       cpuChoice.innerText = `CPU CHOICE: 🧻`;
+      cpuChoice.style.cssText = "background-color: aquamarine";
       result.innerText = `✂ WINS`;
+      result.style.cssText = "background-color: chartreuse";
       playerScore++;
       player.innerText = `PLAYER: ${playerScore}`;
+      player.style.cssText = "background-color: cyan";
       cpu.innerText = `CPU: ${cpuScore}`;
+      cpu.style.cssText = "background-color: violet";
       break;
 
-    case playerSelection.toLowerCase() == "paper" &&
-      computerSelection == "scissor":
+    case playerSelection == "paper" && computerSelection == "scissor":
+      start.innerText = "";
+      start.style.cssText = "background: none";
+      start.style.cssText = "background: none";
+      playerChoice.style.cssText = "background-color: aqua";
       playerChoice.innerText = `PLAYER CHOICE: 🧻`;
       cpuChoice.innerText = `CPU CHOICE: ✂`;
+      cpuChoice.style.cssText = "background-color: aquamarine";
       result.innerText = `✂ WINS`;
+      result.style.cssText = "background-color: chartreuse";
       cpuScore++;
       player.innerText = `PLAYER: ${playerScore}`;
+      player.style.cssText = "background-color: cyan";
       cpu.innerText = `CPU: ${cpuScore}`;
+      cpu.style.cssText = "background-color: violet";
       break;
   }
 
@@ -160,4 +226,8 @@ btnScissor.addEventListener("click", () => {
   let userInput = "scissor";
 
   playRound(userInput, getComputerChoice("rock", "paper", "scissor"));
+});
+
+btnRestart.addEventListener("click", () => {
+  window.location.reload();
 });
